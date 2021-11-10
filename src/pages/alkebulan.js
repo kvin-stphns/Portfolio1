f91ffeeb8014d5292b9f98c0266740cd17220b5e
kvin-stphns <kvin.stphns@gmail.com>
2021-11-09 20:03:57 -0500

    updated imports

--- src/pages/alkebulan.js
+++ src/pages/alkebulan.js
@@ -1,14 +1,14 @@
 import React from "react"
 
-import Layout from "/Users/cozykev/Desktop/NucampFolder/Portfolio/Gatsby/Portfolio1/src/components/layout.js"
+import Layout from "../components/layout.js"
 
 // Components
-import Footer from "/Users/cozykev/Desktop/NucampFolder/Portfolio/Gatsby/Portfolio1/src/components/Footer.js"
+import Footer from "../components/Footer.js"
 
 const alkebulan = () => (
   <Layout>
 
-    
+
     <Footer></Footer>
   </Layout>
 )
