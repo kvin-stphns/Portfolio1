5aee4b86baf568baf2908ab60e3b84c128cf3485
kvin-stphns <kvin.stphns@gmail.com>
2024-09-30 05:02:43 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
           <Fade bottom cascade>
             <h1>Contact</h1>
             <h2>{data.contactSubHeading}</h2>
-            <h2>{data.contactSubHeadingTwo}</h2>
+            <h2 style={{ marginTop: '0.5rem', whiteSpace: 'nowrap' }}>{data.contactSubHeadingTwo}</h2>
           </Fade>
           <a className="email-link" href={`mailto:${data.contactEmail}`}>
             {data.contactEmail}
